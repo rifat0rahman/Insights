833fe54038dcaec0e179dc27460ab5e28d798561
Rifat Rahman <69579356+rifat0rahman@users.noreply.github.com>
2021-06-02 01:28:35 +0600

    Second commit and ready for production to heroku
    I just made some file added to this project so that I can deploy that to.

--- Analysis/Analysis/Analysis/settings.py
+++ Analysis/Analysis/Analysis/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)po79(!r31u9+)ayx)o3*#-(8m6g6rgz)#ijmnz%)vbc)twgtw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- Analysis/Analysis/Analysis/urls.py
+++ Analysis/Analysis/Analysis/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
-
+from django.views.static import serve 
 urlpatterns = [
     path('',include("Timeline.urls")),
     path('admin/', admin.site.urls),

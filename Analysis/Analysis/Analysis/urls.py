ca840142103668fa9e617b27f0ad0978a1235c6d
Rifat Rahman <69579356+rifat0rahman@users.noreply.github.com>
2021-06-02 02:01:25 +0600

    fourth attempt
    edit procfile.

--- Analysis/Analysis/Analysis/urls.py
+++ Analysis/Analysis/Analysis/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
-from django.views.static import serve 
+from django.views.static import serve
+
 urlpatterns = [
     path('',include("Timeline.urls")),
     path('admin/', admin.site.urls),
+    
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
